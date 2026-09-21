c4b5d80798cea46b39f8711adfeaf707aca3d100
Dan Bornstein <danfuzz@murtbo.com>
2026-09-21 15:50:19 -0700

    Remove `RecursiveValueVisitor.ts`.

--- packages/data-model/src/value-visit/index.ts
+++ packages/data-model/src/value-visit/index.ts
@@ -13,6 +13,6 @@
 
 export * from "./interface.ts";
 export { BaseValueVisitor } from "./BaseValueVisitor.ts";
-export { RecursiveValueVisitor } from "./RecursiveValueVisitor.ts";
+export { DefaultValueVisitor } from "./DefaultValueVisitor.ts";
 
 export { makeVisitValueFunction, visitValue } from "./impl.ts";
