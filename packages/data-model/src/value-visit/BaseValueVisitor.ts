71239fe3de9860217bd167e1e98dc17a582f9eb0
Dan Bornstein <danfuzz@murtbo.com>
2026-09-22 16:06:39 -0700

    Clarify.

--- packages/data-model/src/value-visit/BaseValueVisitor.ts
+++ packages/data-model/src/value-visit/BaseValueVisitor.ts
@@ -70,6 +70,10 @@ export abstract class BaseValueVisitor<
    *
    * If not overridden, this returns `true`, thereby corresponding with the
    * default binding for the result type to `FabricValuePlus<PlusType>`.
+   *
+   * Any visitor with a non-default `PlusType` or `ResultType` _must_ override
+   * this method if the default could cause the engine to return a type-lying
+   * value _and_ the calling client cares about avoiding type lies.
    */
   isDomainAssignableToResultType(): boolean {
     return true;
@@ -80,6 +84,9 @@ export abstract class BaseValueVisitor<
    *
    * If not overridden, this returns `false`, thereby corresponding with the
    * default binding for the `PlusType` parameter to `never`.
+   *
+   * Any visitor with a non-default `PlusType` must override this method if it
+   * is to ever receive `PlusType` values.
    */
   isPlusType(_value: unknown): _value is PlusType {
     return false;
@@ -90,6 +97,10 @@ export abstract class BaseValueVisitor<
    *
    * If not overridden, this returns `true`, thereby corresponding with the
    * default binding for the result type to `FabricValuePlus<PlusType>`.
+   *
+   * Any visitor with a non-default `PlusType` or `ResultType` _must_ override
+   * this method if the default could cause the engine to return a type-lying
+   * value _and_ the calling client cares about avoiding type lies.
    */
   isResultType(
     _value: FabricValuePlus<PlusType> | FabricValuePlus<ResultType>,
